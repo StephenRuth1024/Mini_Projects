0f4dc3427aa70cdd8f1ea06d58549362f0344aec
StephenRuth1024 <162595833+StephenRuth1024@users.noreply.github.com>
2024-03-12 22:49:41 +0000

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,4 +1,5 @@
 import operator
+from replit import clear
 
 
 
@@ -16,11 +17,12 @@ def calculator(num1, op, num2):
     print(f"{num1} {op} {num2} = {result}")
     decision = input(f"Type 'y' to continue calculating with {result}, or type 'n' to start a new calculation: ")
     if decision == 'n':
+        clear()
         calculate_new()
     else:
         print("+\n", "-\n", "*\n", "/\n")
         operation = input("Pick an operation: ")
-        num2 = int(input("What's the next number?: "))
+        num2 = float(input("What's the next number?: "))
         calculator(num1=result, op=operation, num2=num2)
 
 
@@ -32,10 +34,10 @@ def calculator(num1, op, num2):
 
 
 def calculate_new():
-    first_number = int(input(f"What's the first number?: "))
+    first_number = float(input(f"What's the first number?: "))
     print("+\n", "-\n", "*\n", "/\n")
     operation = input("Pick an operation: ")
-    second_number = int(input("What's the next number?: "))
+    second_number = float(input("What's the next number?: "))
     calculator(num1=first_number, op=operation, num2=second_number)
 
 
