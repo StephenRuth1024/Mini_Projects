9cb2e5183c91dd47eb004d15657ef28c46accc5f
StephenRuth1024 <162595833+StephenRuth1024@users.noreply.github.com>
2024-03-12 21:57:40 +0000

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,46 @@
+import operator
+
+
+
+operators = {
+        "+": operator.add,
+        "-": operator.sub,
+        "*": operator.mul,
+        "/": operator.truediv
+    }
+
+
+def calculator(num1, op, num2):
+    operation = operators[op]
+    result = operation(num1, num2)
+    print(f"{num1} {op} {num2} = {result}")
+    decision = input(f"Type 'y' to continue calculating with {result}, or type 'n' to start a new calculation: ")
+    if decision == 'n':
+        calculate_new()
+    else:
+        print("+\n", "-\n", "*\n", "/\n")
+        operation = input("Pick an operation: ")
+        num2 = int(input("What's the next number?: "))
+        calculator(num1=result, op=operation, num2=num2)
+
+
+
+
+
+
+
+
+
+def calculate_new():
+    first_number = int(input(f"What's the first number?: "))
+    print("+\n", "-\n", "*\n", "/\n")
+    operation = input("Pick an operation: ")
+    second_number = int(input("What's the next number?: "))
+    calculator(num1=first_number, op=operation, num2=second_number)
+
+
+
+
+
+calculate_new()
+
